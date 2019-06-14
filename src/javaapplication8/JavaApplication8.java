891a577fabb1e85a69376a31e0d18d7b7f31c28e
Owner <Owner@HarshdeepKaur>
2019-06-14 11:12:38 -0400

    harsh bsssss

--- src/javaapplication8/JavaApplication8.java
+++ src/javaapplication8/JavaApplication8.java
@@ -10,6 +10,8 @@ package javaapplication8;
  * @author Owner
  */
 public class JavaApplication8 {
+    
+    private String name ;
 
     /**
      * @param args the command line arguments
@@ -17,6 +19,20 @@ public class JavaApplication8 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("menu kuch ni aunda");
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
     }
     
 }
