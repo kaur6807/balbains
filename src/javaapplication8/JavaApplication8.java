8ccdd5d9a27f8a3632568f51b630ad71b0aa87a6
Owner <Owner@HarshdeepKaur>
2019-06-14 11:42:22 -0400

    batttti khanii

--- src/javaapplication8/JavaApplication8.java
+++ src/javaapplication8/JavaApplication8.java
@@ -18,7 +18,7 @@ public class JavaApplication8 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("menu kuch ni aunda bss tu c rehndo slatetiii");
+        System.out.println("menu kuch ni aunda");
     }
 
     /**
